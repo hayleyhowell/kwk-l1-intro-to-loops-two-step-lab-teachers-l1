a2583cfc9325d5148a2ebf349838bcee7ad03bb8
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-19 18:03:23 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -21,11 +21,11 @@ def a_few_more_steps
   sleep(0.5)
   puts "Stop"
   sleep(1)
-  puts "Right foot back"
+  puts "Right foot steps right and back"
   sleep(0.5)
-  puts "Left foot back"
+  puts "Left foot crosses over right"
   sleep(0.5)
-  puts "Right foot back"
+  puts "Right foot steps right"
   sleep(0.5)
   puts "Stop"
   sleep(1)
